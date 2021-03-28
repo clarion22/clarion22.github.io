46b578b4a8a53fe97378310e50fcd0d5af8228f4
Olga Thompson <olgatj2007@gmail.com>
2021-03-27 23:07:39 -0400

    projects;

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -30,8 +30,8 @@ function Projects() {
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              title="coming soon"
+              description="coming soon"
               link="https://github.com/soumyajit4419/Plant_AI"
             />
           </Col>
@@ -41,7 +41,7 @@ function Projects() {
               imgPath={editor}
               isBlog={false}
               title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              description=""
               link="https://github.com/soumyajit4419/Editor.io"
             />
           </Col>
@@ -50,8 +50,8 @@ function Projects() {
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              title=""
+              description=""
               link="https://github.com/soumyajit4419/AI_For_Social_Good"
             />
           </Col>
@@ -60,9 +60,8 @@ function Projects() {
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              title=""
+              description=""
               link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
             />
           </Col>
@@ -71,13 +70,13 @@ function Projects() {
             <ProjectCard
               imgPath={eeg}
               isBlog={false}
-              title="Emotion Predcition from EEG Signals"
-              description="Using EEG signals from 'AMIGOS' Dataset to extract features and train a classifier that helps in detection of human emotions from Brain Signals."
+              title=""
+              description=""
               link="https://gist.github.com/soumyajit4419/dc94177bc4954752051798f88fbce1df"
             />
           </Col>
         </Row>
-        <h1 className="project-heading">
+        {/* <h1 className="project-heading">
           My Recent <strong className="purple">Blog </strong> Posts
         </h1>
         <p style={{ color: "white" }}>Do give a read to some of my blogs</p>
@@ -98,7 +97,7 @@ function Projects() {
               site="medium.com"
             />
           </Col>
-        </Row>
+        </Row> */}
       </Container>
     </Container>
   );
