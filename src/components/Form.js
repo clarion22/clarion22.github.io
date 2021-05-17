ccc3cf0a81446684bdc827a6e0ad4e92097a6013
Olga Thompson <olgatj2007@gmail.com>
2021-05-17 19:00:29 -0400

    added form and send to email

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,40 +1,76 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import emailjs from 'emailjs-com';
 import '../style.css';
+import { init } from 'emailjs-com';
+init('user_RN3Kx39GSxwLCXr4TfTDU');
 
 function Form() {
-	const [email, setEmail] = useState('');
-	const [message, setMessage] = useState('');
+	const [toSend, setToSend] = useState({
+		from_name: '',
+		email: '',
+		message: '',
+	});
 
 	const onSubmit = (e) => {
 		e.preventDefault();
+
+		emailjs.send('contact_form', 'template_ycyvhgw', toSend).then(
+			(result) => {
+				console.log(result.text);
+			},
+			(error) => {
+				console.log(error.text);
+			}
+		);
 	};
 
+	const handleChange = (e) => {
+		setToSend({ ...toSend, [e.target.name]: e.target.value });
+	};
 	return (
 		<Container fluid className='form-section'>
 			<Container>
-				<form>
+				<form onSubmit={onSubmit}>
 					<div className='mb-3'>
-						<label for='exampleInputEmail1' className='form-label'>
+						<label for='contameform-email' className='form-label'>
+							Name
+						</label>
+						<input
+							type='text'
+							className='form-control'
+							name='from_name'
+							id='contactmeform-name'
+							aria-describedby='emailHelp'
+							onChange={handleChange}
+						/>
+					</div>
+					<div className='mb-3'>
+						<label
+							htmlFor='contameform-email'
+							className='form-label'
+						>
 							Email address
 						</label>
 						<input
 							type='email'
+							name='email'
 							className='form-control'
 							id='contactmeform-email'
 							aria-describedby='emailHelp'
-							onChange={(e) => setEmail(e.target.value)}
+							onChange={handleChange}
 						/>
 					</div>
 					<div class='mb-3'>
-						<label for='form-textarea' className='form-label'>
+						<label htmlFor='form-textarea' className='form-label'>
 							Message
 						</label>
 						<textarea
 							className='form-control'
 							id='form-textarea'
 							rows='3'
-							onChange={(e) => setMessage(e.target.value)}
+							onChange={handleChange}
+							name='message'
 						></textarea>
 					</div>
 					<button type='submit' className='btn btn-primary'>
